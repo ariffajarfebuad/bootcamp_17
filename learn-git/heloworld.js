63806d1071632edff9134ac445620b0293e34e65
arif fajarfeb <>
2025-12-03 10:07:30 +0700

    biodata

---
+++ learn-git/heloworld.js
@@ -0,0 +1,8 @@
+console.info("Hello, World!");
+console.log("=== BIODATA ===");
+console.log("Nama       : Arif Fajar Febriyanto");
+console.log("Umur       : 24 Tahun");
+console.log("Alamat     : cilacap, Jawa Tengah");
+console.log("Profesi    : ui ux designer");
+console.log("Hobi       : badminton, traveling, membaca buku");
+console.log("=================");
