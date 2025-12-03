1651a6f11710f1e536c443558ca02f9edb76f56b
arif fajarfeb <>
2025-12-03 11:22:10 +0700

    bidata

--- learn-git/biodata.js
+++ learn-git/biodata.js
@@ -1,8 +1,51 @@
-console.info("Hello, World!");
-console.log("=== BIODATA ===");
-console.log("Nama       : Arif Fajar Febriyanto");
-console.log("Umur       : 24 Tahun");
-console.log("Alamat     : cilacap, Jawa Tengah");
-console.log("Profesi    : ui ux designer");
-console.log("Hobi       : badminton, traveling, membaca buku");
-console.log("=================");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const biodata = {};
+
+rl.question("Nama lengkap: ", (namaLengkap) => {
+  biodata.namaLengkap = namaLengkap;
+
+  rl.question("Nama panggilan: ", (namaPanggilan) => {
+    biodata.namaPanggilan = namaPanggilan;
+
+    rl.question("Tempat dan tanggal lahir: ", (ttl) => {
+      biodata.ttl = ttl;
+
+      rl.question("Jenis kelamin: ", (jk) => {
+        biodata.jenisKelamin = jk;
+
+        rl.question("Nomor telepon: ", (telepon) => {
+          biodata.telepon = telepon;
+
+          rl.question("Email: ", (email) => {
+            biodata.email = email;
+
+            rl.question("Alamat: ", (alamat) => {
+              biodata.alamat = alamat;
+
+              console.log(`
+
+Nama Lengkap           : ${biodata.namaLengkap}
+Nama Panggilan         : ${biodata.namaPanggilan}
+Tempat & Tanggal Lahir : ${biodata.ttl}
+Jenis Kelamin          : ${biodata.jenisKelamin}
+Nomor Telepon          : ${biodata.telepon}
+Email                  : ${biodata.email}
+Alamat                 : ${biodata.alamat}
+
+
+              `);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
